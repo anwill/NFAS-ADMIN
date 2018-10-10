673102d8c9e96418658ac89d967362915d8e66ef
Andy Williams <a.williams1969@gmail.com>
2018-10-10 13:55:09 +0100

    login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,99 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<!DOCTYPE html>
+<html lang="en">
 
-<div id="infoMessage"><?php echo $message;?></div>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Material Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../../node_modules/mdi/css/materialdesignicons.min.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="../../css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="../../images/favicon.png" />
+</head>
 
-<?php echo form_open("auth/login");?>
+<body>
+<div class="body-wrapper">
+    <div class="page-wrapper">
+        <main class="content-wrapper auth-screen">
+            <div class="mdc-layout-grid">
+                <div class="mdc-layout-grid__inner">
+                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-4">
+                        <div id="infoMessage"><?php echo $message;?></div>
+                    </div>
+                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-4">
+                        <div class="mdc-card">
+                            <section class="mdc-card__primary bg-white">
+                                <form>
+                                    <div class="mdc-layout-grid">
+                                        <div class="mdc-layout-grid__inner">
+                                            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                                                <label class="mdc-text-field w-100">
+                                                    <input type="text" name="identity" value="" id="identity" class="mdc-text-field__input">
+                                                    <span class="mdc-text-field__label">Username</span>
+                                                    <div class="mdc-text-field__bottom-line"></div>
+                                                </label>
+                                            </div>
+                                            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                                                <label class="mdc-text-field w-100">
+                                                    <input type="password" name="password" value="" id="password" class="mdc-text-field__input">
+                                                    <span class="mdc-text-field__label">Password</span>
+                                                    <div class="mdc-text-field__bottom-line"></div>
+                                                </label>
+                                            </div>
+                                            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
+                                                <div class="mdc-form-field">
+                                                    <div class="mdc-checkbox">
+                                                        <input type="checkbox" name="remember" value="1"  id="remember" class="mdc-checkbox__native-control"/>
+                                                        <div class="mdc-checkbox__background">
+                                                            <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+                                                                <path class="mdc-checkbox__checkmark__path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+                                                            </svg>
+                                                            <div class="mdc-checkbox__mixedmark"></div>
+                                                        </div>
+                                                    </div>
+                                                    <label for="my-checkbox">Remember me</label>
+                                                </div>
+                                            </div>
+                                            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6 d-flex align-item-center justify-content-end">
+                                                <a href="#">Forgot Password</a>
+                                            </div>
+                                            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                                                <button class="mdc-button mdc-button--raised w-100" data-mdc-auto-init="MDCRipple">
+                                                    Login
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </section>
+                        </div>
+                    </div>
+                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-4">
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+<!-- body wrapper -->
+<!-- plugins:js -->
+<script src="../../node_modules/material-components-web/dist/material-components-web.min.js"></script>
+<script src="../../node_modules/jquery/dist/jquery.min.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="../../js/misc.js"></script>
+<script src="../../js/material.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<!-- End custom js for this page-->
+</body>
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
-
-
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
-
-<?php echo form_close();?>
-
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+</html>
