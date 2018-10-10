c2c14b65a52dfc9f3975301078439f160e5154f4
Andy Williams <a.williams1969@gmail.com>
2018-10-10 16:47:17 +0100

    link user with club

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -19,7 +19,7 @@ class Booking extends CI_Controller {
     {
         if ($this->ion_auth->logged_in()) {
             $data = Array();
-            var_dump($this->ion_auth->user()->row());
+            var_dump($this->session->userdata);
             $this->_render_page('home', $data);
         } else {
             $this->load->view('auth/login');
