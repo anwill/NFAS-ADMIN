8e22768fd7d6e83bb07f66d297a83592d88e9019
Andy Williams <a.williams1969@gmail.com>
2018-10-10 14:38:22 +0100

    header / footer

---
+++ application/views/footer.php
@@ -0,0 +1,35 @@
+<!-- partial:partials/_footer.html -->
+<footer>
+    <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__inner">
+            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
+                <span class="text-muted">Copyright © 2018 <a class="text-green" href="https://www.bootstrapdash.com/" target="_blank">Bootstrap Dash</a>. All rights reserved.</span>
+            </div>
+            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6 d-flex justify-content-end">
+                <span class="mt-0 text-right">Hand-crafted &amp; made with <i class="mdi mdi-heart text-red"></i></span>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- partial -->
+</div>
+</div>
+<!-- body wrapper -->
+<!-- plugins:js -->
+<script src="../../node_modules/material-components-web/dist/material-components-web.min.js"></script>
+<script src="../../node_modules/jquery/dist/jquery.min.js"></script>
+<!-- endinject -->
+<!-- Plugin js for this page-->
+<script src="../../node_modules/chart.js/dist/Chart.min.js"></script>
+<script src="../../node_modules/progressbar.js/dist/progressbar.min.js"></script>
+<!-- End plugin js for this page-->
+<!-- inject:js -->
+<script src="../../js/misc.js"></script>
+<script src="../../js/material.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="../../js/dashboard.js"></script>
+<!-- End custom js for this page-->
+</body>
+
+</html>
