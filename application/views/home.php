b43a96b8c83f1495fa2938e725dc314c0c23f141
Andy Williams <a.williams1969@gmail.com>
2018-10-11 09:14:27 +0100

    link user with club

--- application/views/home.php
+++ application/views/home.php
@@ -1,16 +1,17 @@
 
     <div class="page-wrapper mdc-toolbar-fixed-adjust">
         <main class="content-wrapper">
-            <div class="mdc-card">
-                <div class="mdc-layout-grid__inner">
-                    <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
-                        <section class="purchase__card_section">
-                            <pre><?php var_dump($club); ?></pre>
-                        </section>
+            <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                <div class="mdc-card">
+                    <div class="mdc-layout-grid__inner">
+                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                            <section class="purchase__card_section">
+                                <pre><?php var_dump($club); ?></pre>
+                            </section>
+                        </div>
                     </div>
                 </div>
             </div>
-            
             <div class="mdc-layout-grid">
                 <div class="mdc-layout-grid__inner">
 
