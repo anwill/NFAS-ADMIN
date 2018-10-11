d7772ebc8807c35eb2a6232d114f137027a0be00
Andy Williams <a.williams1969@gmail.com>
2018-10-11 10:34:23 +0100

    forgot password

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -30,7 +30,7 @@
                             <section class="mdc-card__primary bg-white">
                                 <div class="mdc-layout-grid">
                                     <div class="mdc-layout-grid__inner">
-                                        <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
+                                        <div class="mdc-persistent-drawer__toolbar-spacer mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-12">
                                             <a href="/index.php/booking/" class="brand-logo">
                                                 <img src="../../images/tmp_logo" alt="logo">
                                             </a>
