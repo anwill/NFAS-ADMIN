bdd0ede7b217a0ea0d818c28548e85d22867ef1b
Andy Williams <a.williams1969@gmail.com>
2018-10-15 10:59:02 +0100

    add view shoot

--- application/views/footer.php
+++ application/views/footer.php
@@ -13,29 +13,6 @@
 </div>
 </div>
 
-<?php if (isset($_SESSION['message'])) { ?>
-<div class="mdc-dialog"
-     role="alertdialog"
-     aria-modal="true"
-     aria-labelledby="my-dialog-title"
-     aria-describedby="my-dialog-content">
-    <div class="mdc-dialog__container">
-        <div class="mdc-dialog__surface">
-            <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
-            <h2 class="mdc-dialog__title" id="my-dialog-title"><!--
-     -->Error Occurred<!--
-   --></h2>
-            <div class="mdc-dialog__content" id="my-dialog-content">
-                <?php echo $_SESSION['message']; ?>
-            </div>
-            <footer class="mdc-dialog__actions">
-                <button type="button" class="mdc-button mdc-dialog__button" data-mdc-dialog-action="close">Close</button>
-            </footer>
-        </div>
-    </div>
-    <div class="mdc-dialog__scrim"></div>
-</div>
-<?php } ?>
 
 <!-- body wrapper -->
 <!-- plugins:js -->
@@ -53,6 +30,38 @@
 <!-- Custom js for this page-->
 <script src="../../js/dashboard.js"></script>
 <!-- End custom js for this page-->
+
+<?php if (isset($_SESSION['message'])) { ?>
+    <div class="mdc-dialog"
+         id="alert-modal"
+         role="alertdialog"
+         aria-modal="true"
+         aria-labelledby="my-dialog-title"
+         aria-describedby="my-dialog-content">
+        <div class="mdc-dialog__container">
+            <div class="mdc-dialog__surface">
+                <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+                <h2 class="mdc-dialog__title" id="my-dialog-title"><!--
+     -->Error Occurred<!--
+   --></h2>
+                <div class="mdc-dialog__content" id="my-dialog-content">
+                    <?php echo $_SESSION['message']; ?>
+                </div>
+                <footer class="mdc-dialog__actions">
+                    <button type="button" class="mdc-button mdc-dialog__button" data-mdc-dialog-action="close">Close</button>
+                </footer>
+            </div>
+        </div>
+        <div class="mdc-dialog__scrim"></div>
+    </div>
+    <script>
+        var dialog = new mdc.dialog.MDCDialog(document.querySelector('#alert-modal'));
+        dialog.show();
+
+    </script>
+<?php } ?>
+
+
 </body>
 
 </html>
