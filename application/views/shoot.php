6c8a94d500ff6f55b038e6f6cc9e1fdf44eaa5a4
Andy Williams <a.williams1969@gmail.com>
2018-10-16 09:12:25 +0100

    shoot page

--- application/views/shoot.php
+++ application/views/shoot.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="/css/datepicker.css">
 <div class="page-wrapper mdc-toolbar-fixed-adjust">
     <main class="content-wrapper">
 
@@ -42,13 +43,13 @@
                                 Start Date
                             </div>
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-4">
-                                <md-datepicker v-model="selectedDate" md-immediately />
+                                <input name='date_from' class="c-datepicker-input-from" />
                             </div>
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-2">
                                 End Date
                             </div>
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-4">
-                                <md-datepicker v-model="selectedDate" md-immediately />
+                                <input name='date_to' class="c-datepicker-input-to" />
                             </div>
                         </div>
                     </div>
@@ -58,7 +59,7 @@
     </main>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-
+    <script src="/js/material-datetime-picker.js"></script>
     <script>
         $(document).ready(function(){
             $('#delete-button').click(function () {
@@ -70,5 +71,25 @@
                 window.location.href = '/index.php/booking/view_shoot/' + $(this).data('id');
             }) ;
 
+
         });
+
+        import MaterialDateTimePicker from 'material-datetime-picker';
+
+        const from = document.querySelector('.c-datepicker-input-from');
+        const from_picker = new MaterialDateTimePicker()
+            .on('submit', (val) => {
+                from.value = val.format("YYYY/MM/DD");
+            });
+
+        from.addEventListener('focus', () => from_picker.open());
+
+        const to = document.querySelector('.c-datepicker-input-to');
+        const to_picker = new MaterialDateTimePicker()
+            .on('submit', (val) => {
+                to.value = val.format("YYYY/MM/DD");
+            });
+
+        to.addEventListener('focus', () => to_picker.open());
+
     </script>
