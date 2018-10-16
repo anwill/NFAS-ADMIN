6bb10a500ab80d5fc7fcdcdc8cd8d9149f49c4b9
Andy Williams <a.williams1969@gmail.com>
2018-10-16 09:59:14 +0100

    shoot page

--- application/views/shoot.php
+++ application/views/shoot.php
@@ -42,14 +42,17 @@
                     <div class="mdc-card">
                         <div class="mdc-layout-grid__inner">
                             <div class="mdc-layout-grid__cell stretch-card mdc-layout-grid__cell--span-6">
-                                <div class="mdc-text-field mdc-text-field--fullwidth mdc-text-field--box w-100 mdc-text-field--upgraded mdc-ripple-upgraded mdc-ripple-upgraded--background-active-fill mdc-ripple-upgraded--foreground-activation mdc-text-field--invalid" style="--mdc-ripple-fg-size:208.387px; --mdc-ripple-fg-scale:1.73618; --mdc-ripple-fg-translate-start:34.3258px, -66.7172px; --mdc-ripple-fg-translate-end:69.4625px, -76.1937px;">
+                                <div id="tf-box-example" class="mdc-text-field mdc-text-field--box w-100 mdc-text-field--upgraded mdc-ripple-upgraded mdc-text-field--invalid mdc-text-field--focused" style="--mdc-ripple-fg-size:208.387px; --mdc-ripple-fg-scale:1.73618; --mdc-ripple-fg-translate-start:58.6383px, -75.7875px; --mdc-ripple-fg-translate-end:69.4625px, -76.1937px;">
                                     <input name="date_start" data-toggle="datepicker"
+                                           required=""
+                                           pattern=".{1,}"
                                            type="text" id="date-start" class="mdc-text-field__input"
+                                           aria-controls="name-validation-message"
                                            value="<?php echo $shoot->date_start; ?>">
-                                    <label class="mdc-text-field__label mdc-text-field__label--shake" for="date-start">
+                                    <label class="mdc-text-field__label mdc-text-field__label--float-above" for="date-start">
                                         Start Date
                                     </label>
-                                    <div class="mdc-line-ripple"></div>
+                                    <div class="mdc-text-field__bottom-line mdc-text-field__bottom-line--active" style="transform-origin: 162px center"></div>
                                 </div>
                             </div>
 
